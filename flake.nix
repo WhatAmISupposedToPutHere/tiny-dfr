7e974e647b4041d4bfe7d0759b106e6b2b54b78e
LawsOfScience <thesandwichsupreme@gmail.com>
2024-03-27 14:05:11 -0400

    Add aarch64-linux back to flake

--- flake.nix
+++ flake.nix
@@ -5,39 +5,56 @@
   };
   outputs = { self, nixpkgs }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in
     rec {
-      packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
-        name = "tiny-dfr";
-        version = "0.2.0";
-        src = ./.;
-        cargoLock = {
-          lockFile = ./Cargo.lock;
-        };
-        nativeBuildInputs = [
-          pkgs.pkg-config
-        ];
-        buildInputs = [
-          pkgs.cairo
-          pkgs.libinput
-          pkgs.freetype
-          pkgs.fontconfig
-          pkgs.glib
-          pkgs.pango
-          pkgs.gdk-pixbuf
-          pkgs.libxml2
-        ];
-      };
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        inputsFrom = [
-          packages.x86_64-linux.default
-        ];
-        packages = [
-	        pkgs.rustfmt
-          pkgs.rust-analyzer
-        ];
-        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
-      };
+      packages = forAllSystems(system: 
+        let
+          pkgs = pkgsFor.${system};
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            name = "tiny-dfr";
+            version = "0.2.0";
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+            nativeBuildInputs = [
+              pkgs.pkg-config
+            ];
+            buildInputs = [
+              pkgs.cairo
+              pkgs.libinput
+              pkgs.freetype
+              pkgs.fontconfig
+              pkgs.glib
+              pkgs.pango
+              pkgs.gdk-pixbuf
+              pkgs.libxml2
+            ];
+          };
+        }
+      );
+
+      devShells = forAllSystems(system:
+        let
+          pkgs = pkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = [
+              packages.${system}.default
+            ];
+            packages = [
+    	        pkgs.rustfmt
+              pkgs.rust-analyzer
+            ];
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          };
+        }
+      );
     };
 }
